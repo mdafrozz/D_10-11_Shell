e5ea563428bdc729d78e983dafffe9a83273cf82
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-07 16:30:28 +0530

    Refactor: Sort the results to show computation in Ascending order

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -54,3 +54,22 @@ do
 	done
 done
 echo "Array in descending order: ${array[@]}"
+
+#UC9:Array in ascending order
+echo "-------------Ascending Order---------------"
+echo "Array : ${array[@]}"
+echo "Array Size: ${#array[@]}"
+
+for(( i=0;i<4;i++ ))
+do
+        for(( j=0;j<4-i-1;j++ ))
+        do
+                if [ ${array[j]} -gt ${array[$((j+1))]} ]
+                then
+                        temp=${array[j]}
+                        array[$j]=${array[$((j+1))]}
+                        array[$((j+1))]=$temp
+                fi
+        done
+done
+echo "Array in ascending order: ${array[@]}"

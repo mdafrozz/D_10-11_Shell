a3bb0bfd3eeafa56fc52b2586d67c36431ec0bff
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-07 15:46:42 +0530

    Add: Take Three Inputs a,b & c

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Day_10-11
+***** Welcome to Sorting Arithmetic Computation Problem
---
+++ SortingArithmeticComputation.sh
@@ -0,0 +1,9 @@
+echo "*********** Welcome to Sorting Arithmetic Computation Poblem **********"
+
+#UC1 Program to take three inputs a, b & c
+
+read -p "Enter a value: " a
+read -p "Enter b value: " b
+read -p "Enter c vakue: " c
+
+echo "Values of a= $a, b= $b and c= $c"

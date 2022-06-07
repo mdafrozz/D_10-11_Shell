41de1c6997cfcd67ecd9b1622e84b4dad751b3f6
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-06-07 16:27:15 +0530

    Refactor: Sort the results to show computation in descending order

--- SortingArithmeticComputation.sh
+++ SortingArithmeticComputation.sh
@@ -36,3 +36,21 @@ echo "Stored data in dictionary : ${storeData[@]}"
 #UC7 : Read values from dictionary into array
 array=("$expression1" "$expression2" "$expression3" "$expression4")
 echo "Array : ${array[@]}"
+
+#UC8: Sort Array in descending order
+echo "Array : ${array[@]}"
+echo "Array Size: ${#array[@]}"
+
+for(( i=0;i<4;i++ ))
+do
+	for(( j=0;j<4-i-1;j++ ))
+	do
+		if [ ${array[j]} -lt ${array[$((j+1))]} ]
+		then
+			temp=${array[j]}
+			array[$j]=${array[$((j+1))]}
+			array[$((j+1))]=$temp
+		fi
+	done
+done
+echo "Array in descending order: ${array[@]}"
